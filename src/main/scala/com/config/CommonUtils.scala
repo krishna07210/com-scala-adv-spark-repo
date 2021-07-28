e1cf2ad6bfa841b6b4f5b42d3956158215394c22
Harikrishna Thariboyina <=>
2021-07-28 19:22:37 +0530

    new version

--- src/main/scala/com/config/CommonUtils.scala
+++ src/main/scala/com/config/CommonUtils.scala
@@ -16,6 +16,9 @@ object CommonUtils {
 
   def getInputFilePath(args: Array[String], fileName: String): String = {
     checkUserInput(args)
+        println("Is Input Argument File -> " + isUserInputFile)
+        println("Input Path -> " + InputPath)
+        println("Output Path -> " + OutputPath)
     if (isUserInputFile.equals("Y")) {
       return InputPath
     } else {
@@ -23,21 +26,26 @@ object CommonUtils {
     }
   }
 
-  def getOutputFilePath(args: Array[String], fileName: String): String = {
-    checkUserInput(args)
-    return OutputPath + fileName
-  }
-
   def checkUserInput(args: Array[String]): Unit = {
+    println("Arguments Length : " + args.length)
     if (args.length > 0) {
+//      println("args[0] : " + args(0))
       if (args(0).toUpperCase.equals("F")) {
         isUserInputFile = "Y"
       }
       if (args.length == 1) {
         InputPath = args(0)
-      } else if (args.length > 1) {
-        InputPath = args(0)
-        OutputPath = args(1)
+      } else if (args.length == 2) {
+        if (args(0).toUpperCase.equals("F") || args(0).toUpperCase.equals("D")) {
+          InputPath = args(1)
+        } else {
+          InputPath = args(1)
+          OutputPath = args(2)
+        }
+      }
+      else if (args.length == 3) {
+        InputPath = args(1)
+        OutputPath = args(2)
       } else {
         InputPath = getInputResourcesPath()
         OutputPath = getOutputResourcesPath
@@ -51,6 +59,11 @@ object CommonUtils {
 
   def getOutputResourcesPath(): String = {
     return getClass.getResource("/trgData/").toString
+  }
+
+  def getOutputFilePath(args: Array[String], fileName: String): String = {
+    checkUserInput(args)
+    return OutputPath + fileName
   }
 
   //  def getInputFilePath(args,fileName: String): String = {
